a4e4d39254de33d494bf29a895c57c1e27856789
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2024-12-18 14:44:35 +0200

    fix httpx version and use logger from Alkemio VC engine lib

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import re
 
 from openai.types import FileObject
 from openai.types.beta.thread_create_params import MessageAttachment
-from logger import setup_logger
+from alkemio_virtual_contributor_engine.setup_logger import setup_logger
 
 logger = setup_logger(__name__)
 
